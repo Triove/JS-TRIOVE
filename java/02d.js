fba01c60b7cb6e9f7fbf8ba1d5c7c31122cb703a
Triove <153115887+Triove@users.noreply.github.com>
2024-06-01 08:24:59 +0000

    div

---
+++ java/02d.js
@@ -0,0 +1,6 @@
+"use strict"; /* user strict function use isliye krte ke jo bi code likhege wo new version of javascript kelye hai q ke aj se 8-9 saal purani or new java me bht farq hai */
+
+let numb = "345"
+let a = Number(numb)
+console.log(typeof (a))
+console.log(typeof numb)
