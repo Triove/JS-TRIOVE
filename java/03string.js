fc7214e5401e029be769e87dfdbf780b8fd1aa80
Triove <153115887+Triove@users.noreply.github.com>
2024-06-01 18:15:50 +0000

    div

--- java/03string.js
+++ java/03string.js
@@ -1,6 +1,9 @@
-let name1 = "hello"
+let name1 = "jawad"
 console.log(name1);
  newname = name1.slice(0,4)
 console.log(newname);
 name2 = name1.toUpperCase()
 console.log(name2);
+// backtick laga ker hum dollar sign with curely bracket dynamic cheese assign kr sakte hai array ke andar
+b1 = `hello ${name2}`
+console.log(b1);
---
+++ java/04numb.js
@@ -0,0 +1,2 @@
+let n1 =  Number(23)
+console.log(n1);
