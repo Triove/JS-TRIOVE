6b04ebc1d81ed9f909324985316705e8ada853ac
Triove <153115887+Triove@users.noreply.github.com>
2024-06-01 07:43:21 +0000

    aconfig

---
+++ java/01.js
@@ -0,0 +1,9 @@
+const ActID = 110104
+let ActName = "Jawad"
+var ActFName = "Bari"
+Balance = "352000"
+let ActState
+
+/* vari ni use krna variable declare krny kelye, Let use krna hai q ke functional scope ki waja */
+console.log(ActID);
+console.table([ActID, ActName, ActFName, Balance,ActState])
